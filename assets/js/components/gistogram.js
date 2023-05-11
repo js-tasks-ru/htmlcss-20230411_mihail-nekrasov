8f0d540bd4db0776d2111b2aa4e167058ad326d3
Михаил Сергеевич Некрасов <MSNekrasov@sberbank.ru>
2023-05-11 10:03:27 +0300

    task_5 remarks of review

--- assets/js/components/gistogram.js
+++ assets/js/components/gistogram.js
@@ -1,12 +1,10 @@
 class Chart {
-  scaleRows = 4;
   constructor(params) {
     this.data = params.data;
     this.type = params.type;
     this.container = params.container;
     this.maxHeight = params.container.clientHeight;
   }
-
   findMaxValue() {
     return this.data.reduce((prev, curr) => {
       if (prev > curr) return prev;
@@ -14,56 +12,23 @@ class Chart {
     }, 0);
   }
 
-  getBarChartEl() {
+  renderBarChart() {
     const maxValue = this.findMaxValue();
     const dataToBarHeight = this.data.map((item) => {
       return item * (this.maxHeight / maxValue);
     });
-    const barChartEl = document.createElement('div');
-    barChartEl.classList.add('histogram__chart');
-
     for (let i = 0; i < dataToBarHeight.length; i++) {
       const barItemEl = document.createElement('div');
       barItemEl.classList.add('histogram__bar');
       barItemEl.style.height = dataToBarHeight[i] + 'px';
-      barChartEl.appendChild(barItemEl);
+      this.container.appendChild(barItemEl);
     }
-    return barChartEl;
-  }
-
-  getScaleEl(maxValue) {
-    const scaleEl = document.createElement('div');
-    scaleEl.classList.add('histogram__scale');
-    for (let i = 0; i < this.scaleRows; i++) {
-      const scaleNumEl = document.createElement('div');
-      scaleNumEl.classList.add('histogram__scale-number');
-      scaleNumEl.textContent = Math.round(maxValue / (i + 1));
-      scaleEl.appendChild(scaleNumEl);
-    }
-    return scaleEl;
-  }
-
-  getChartGridEl() {
-    const chartGridEl = document.createElement('div');
-    chartGridEl.classList.add('histogram__grid');
-    for (let i = 0; i < this.scaleRows; i++) {
-      const gridRowEl = document.createElement('div');
-      gridRowEl.classList.add('histogram__grid-row');
-      chartGridEl.appendChild(gridRowEl);
-    }
-    return chartGridEl;
   }
 
   render() {
     switch (this.type) {
       case 'bar':
-        const scaleEl = this.getScaleEl(this.findMaxValue());
-        const gridChartEl = this.getChartGridEl();
-        const chartEl = this.getBarChartEl();
-        chartEl.appendChild(gridChartEl);
-
-        this.container.appendChild(scaleEl);
-        this.container.appendChild(chartEl);
+        this.renderBarChart();
         break;
     }
   }
